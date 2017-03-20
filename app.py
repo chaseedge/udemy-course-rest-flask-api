e930ea713f4487d7edcec1b642ba381fdf9bcb21
Chase Edge <chaseedge@Chases-MacBook-Air.local>
2017-03-20 15:05:14 -0400

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'
 api = Api(app)
 
-# use sqlachemy to create database
-@app.before_first_request
-def create_tables():
-    db.create_all() # will create all tables from imports unless they already exists
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
